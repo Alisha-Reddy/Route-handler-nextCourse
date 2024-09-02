8b38a4274c663acc46915a958767756ad3681d35
Alisha-Reddy <kondapualishareddy@gmail.com>
2024-09-02 13:00:44 +0530

    redirects

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,12 +1,29 @@
+import { redirect } from "next/navigation"
+import { type NextRequest } from "next/server" 
 import { comments } from "../data"
 
 export async function GET(
-    _request: Request,
-    { params } : {params: {id: string}}
+    _request: Request, 
+    {params}: {params: {id: string}}
 ) {
-    const comment = comments.find((comment) => comment.id === parseInt(params.id))
+    if (parseInt(params.id) > comments.length) {
+        redirect("/comments");
+    }
+    const comment = comments.find(
+        comment => comment.id === parseInt(params.id)
+    )
     return Response.json(comment)
 }
+
+// // URL Query Parameters
+// export async function GET(request: NextRequest) {
+//     const searchParams = request.nextUrl.searchParams
+//     const query = searchParams.get("query")
+//     const filteredComments = query ? comments.filter(comment => comment.text.includes(query)) : comments
+//     return Response.json(filteredComments)
+// }
+
+
 
 export async function PATCH(
     request: Request,
@@ -33,3 +50,4 @@ export async function DELETE(
     return Response.json(deletedComment)
 
 }
+
